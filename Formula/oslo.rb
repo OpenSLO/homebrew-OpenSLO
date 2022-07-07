1dbe2ae0ff654fb87957efae140a66b7850021b2
goreleaserbot <goreleaser@carlosbecker.com>
2022-07-07 14:43:09 -0700

    Brew formula update for oslo version v0.5.1

--- Formula/oslo.rb
+++ Formula/oslo.rb
@@ -5,43 +5,43 @@
 class Oslo < Formula
   desc "Command line tool for working with OpenSLO"
   homepage "https://github.com/OpenSLO/oslo"
-  version "0.5.0"
+  version "0.5.1"
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.0/oslo_Darwin_x86_64.tar.gz"
-      sha256 "2ecfa5ad5b61cf72919c11208e993a36a4a743481ddcb5a8d899cf81b90edbb9"
+    if Hardware::CPU.arm?
+      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.1/oslo_Darwin_arm64.tar.gz"
+      sha256 "2263ab82b5891fcefffa6727e8b7f13cae12164f99e5dd414c9adc09c97c2463"
 
       def install
-        bin.install "oslo"
+        bin.install "bin/oslo"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.0/oslo_Darwin_arm64.tar.gz"
-      sha256 "d0029413fa4d335ce826552df512187d55911578a0fbd45812bbe706ba6a4b78"
+    if Hardware::CPU.intel?
+      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.1/oslo_Darwin_x86_64.tar.gz"
+      sha256 "aa9eb88292e6d605f8c23b0be3ad86e54565473f2b255d53ec4875a1178e278e"
 
       def install
-        bin.install "oslo"
+        bin.install "bin/oslo"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.0/oslo_Linux_x86_64.tar.gz"
-      sha256 "13f287eb88c1c5ea1751779691180c2280cdff221205b99811cecb784965ef4e"
+      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.1/oslo_Linux_x86_64.tar.gz"
+      sha256 "68475e21d95ba7f66b195821f3a0c07ac7797f0208cef36d80c7ac71a75b1b91"
 
       def install
-        bin.install "oslo"
+        bin.install "bin/oslo"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.0/oslo_Linux_arm64.tar.gz"
-      sha256 "dc825146eb1bc5c1fbc5196adb40778d8598d89d8bedaf22b4513518cfa5429b"
+      url "https://github.com/OpenSLO/oslo/releases/download/v0.5.1/oslo_Linux_arm64.tar.gz"
+      sha256 "3bf272d280bcf911d702cf48bd0550115edf07f8c58e24e528278cfc46e346f6"
 
       def install
-        bin.install "oslo"
+        bin.install "bin/oslo"
       end
     end
   end
